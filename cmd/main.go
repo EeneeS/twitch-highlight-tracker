9558aa7165a46bd81c3f387cfadeff302dd6fe7c
Niels Soete <soeteniels1803@gmail.com>
2025-02-26 22:13:20 +0100

    export to package

--- cmd/main.go
+++ cmd/main.go
@@ -1,66 +1,34 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"net"
-	"strings"
+
+	"github.com/eenees/twitch-highlight-tracker/internal/irc"
 )
 
 var (
   server = "irc.chat.twitch.tv:6667"
-  channel = "theprimeagen"
+  channel = "ohnepixel"
 )
 
 func main() {
-  conn, err := net.Dial("tcp", server)
+  client, err:= irc.NewClient(server)
   if err != nil {
-    fmt.Println("failed connection", err)
+    fmt.Println("failed to create client", err)
     return
   }
-  defer conn.Close()
 
-  logon(conn)
-  joinChannel(conn, channel)
+  err = client.Logon()
+  if err != nil {
+    fmt.Println("failed to logon", err)
+    return
+  }
 
-  readLoop(conn)
+  err = client.JoinChannel(channel)
+  if err != nil {
+    fmt.Println("failed to join channel", err)
+    return
+  }
 
-}
-
-func joinChannel(conn net.Conn, channel string) {
-	sendData(conn, fmt.Sprintf("JOIN #%s", channel))
-}
-
-func logon(conn net.Conn) {
-  sendData(conn, "PASS oath:justinfan1234")
-  sendData(conn, "NICK justinfan1234")
-}
-
-func sendData(conn net.Conn, message string) {
-  fmt.Fprintf(conn, "%s\r\n", message)
-}
-
-func readLoop(conn net.Conn) {
-	reader := bufio.NewReader(conn)
-
-	for {
-		message, err := reader.ReadString('\n')
-
-		if err != nil {
-			fmt.Println("Connection closed:", err)
-			return
-		}
-
-    if strings.Contains(message, "PRIVMSG") {
-      parts := strings.Split(message, " :")
-      if len(parts) > 1 {
-        userMessage := parts[1]
-        fmt.Println(userMessage)
-      }
-    }
-
-		if strings.HasPrefix(message, "PING") {
-			sendData(conn, strings.Replace(message, "PING", "PONG", 1))
-		}
-	}
+  client.ReadMessages()
 }
---
+++ internal/irc/client.go
@@ -0,0 +1,64 @@
+package irc
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+)
+
+type Client struct {
+  conn *net.Conn
+}
+
+func NewClient(server string) (*Client, error) {
+  conn, err := net.Dial("tcp", server)
+  if err != nil {
+    fmt.Println("failed connection", err)
+    return nil, err
+  }
+  return &Client{
+    conn: &conn,
+  }, nil
+}
+
+func (c *Client) Logon() error {
+  err := c.SendData("PASS oath:justinfan1234")
+  err = c.SendData("NICK justinfan1234")
+  return err
+}
+
+func (c *Client) SendData(message string) error {
+  _, err := fmt.Fprintf(*c.conn, "%s\r\n", message)
+  return err
+}
+
+func (c *Client) JoinChannel(channel string) error {
+  joinString := fmt.Sprintf("JOIN #%s", channel)
+  err := c.SendData(joinString)
+  return err
+}
+
+func (c *Client) ReadMessages() {
+  reader := bufio.NewReader(*c.conn)
+  for {
+    message, err := reader.ReadString('\n')
+
+    if err != nil {
+      fmt.Println("Connection closed", err)
+      return
+    }
+
+    if strings.Contains(message, "PRIVMSG") {
+      parts := strings.Split(message, " :")
+      if len(parts) > 1 {
+        userMessage := parts[1]
+        fmt.Println(userMessage)
+      }
+    }
+
+    if strings.HasPrefix(message, "PING") {
+      c.SendData(strings.Replace(message, "PING", "PONG", 1))
+    }
+  }
+}
