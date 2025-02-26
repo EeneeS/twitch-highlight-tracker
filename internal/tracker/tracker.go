74f2897a234ddf668aaefdd8e293900891c1cf6f
Niels Soete <soeteniels1803@gmail.com>
2025-02-26 22:47:26 +0100

    create tracker package

---
+++ internal/tracker/tracker.go
@@ -0,0 +1,37 @@
+package tracker
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/eenees/twitch-highlight-tracker/internal/irc"
+)
+
+type Tracker struct {
+  client *irc.Client
+  // this should store the timestamps
+}
+
+func NewTracker(client *irc.Client) *Tracker {
+  return &Tracker{
+    client: client,
+  }
+}
+
+func (t *Tracker) Run() {
+  for {
+    rawMessage, err := t.client.ReadMessage()
+    if err != nil {
+      fmt.Println("failed to read message", err)
+      return
+    }
+
+    message := irc.ParseMessage(rawMessage)
+
+    if strings.HasPrefix(rawMessage, "PING") {
+      t.client.SendData(strings.Replace(rawMessage, "PING", "PONG", 1))
+    }
+
+    fmt.Println(message.Text)
+  }
+}
