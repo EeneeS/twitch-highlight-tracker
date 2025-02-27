83b4e97358c8a3b01040cd1bde15ce4e09d6bc1b
Niels Soete <soeteniels1803@gmail.com>
2025-02-27 14:25:59 +0100

    create package for viewer functionality
    created basic functionality

---
+++ internal/tracker/viewers.go
@@ -0,0 +1,30 @@
+package tracker
+
+import (
+	"fmt"
+	"time"
+)
+
+func (t *Tracker) UpdateViewerCount() {
+	for {
+		viewerCount, err := t.fetchViewerCountFromAPI()
+		if err != nil {
+			fmt.Println("failed to update viewercount") // maybe fall back to the previous fetched value (if any or >0?)
+			return
+		}
+		t.viewerLock.Lock()
+		t.viewerCount = viewerCount
+		t.viewerLock.Unlock()
+		time.Sleep(time.Minute * 10)
+	}
+}
+
+func (t *Tracker) fetchViewerCountFromAPI() (int, error) {
+	return 1000, nil
+}
+
+func (t *Tracker) GetViewerCount() int {
+	t.viewerLock.RLocker()
+	defer t.viewerLock.RUnlock()
+	return t.viewerCount
+}
