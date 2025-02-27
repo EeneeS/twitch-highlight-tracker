36378e57165803c3be97251bc1956d7b189be0b4
Niels Soete <soeteniels1803@gmail.com>
2025-02-27 09:11:11 +0100

    add keywords to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,11 +3,13 @@ package config
 type Config struct {
   Server string
   Channel string
+  Keywords []string
 }
 
 func LoadConfig() *Config {
   return &Config{
     Server: "irc.chat.twitch.tv:6667",
     Channel: "eenees_",
+    Keywords: []string{"LOL", "OOOOO"},
   }
 }
